47111d16aa30391d17d3f3e3b4e6115e46091963
IT-IT-is-a-Trip <alexey135135025@gmail.com>
2024-12-07 01:51:00 +0300

    Experemental DOM moves

--- js.js
+++ js.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let ComputerScore = 0;
+let rounds = 0;
 function getComputerChoice(randomChoice) {
     let rand = Math.random(randomChoice);
     let randResult;
@@ -15,46 +18,69 @@ function getComputerChoice(randomChoice) {
 }
 
 
-function getHumanChoice() {
-    let enterChoice = prompt('Rock, Paper, Scissors?', "").toLowerCase();
-    console.log("Your choice: " + enterChoice);
-    return enterChoice;
+// function getHumanChoice() {
+    //     let enterChoice = prompt('Rock, Paper, Scissors?', "").toLowerCase();
+    //     console.log("Your choice: " + enterChoice);
+    //     return enterChoice;
+    // } WILL BE REMOVE
+    let wonRound = "You victory!";
+    let loseRound = "Computer victory!";
+    let tieRound = 'Tie, try again';
+    const rockb = document.querySelector('#rock');
+    const paperb = document.querySelector('#paper');
+    const scissorsb = document.querySelector('#scissors');
+    const restartb = document.querySelector('#restart');
+    restartb.addEventListener('click', () => {
+        rounds = 0;
+        humanScore = 0;
+        ComputerScore = 0;
+    })
+    
+    rockb.addEventListener("click", () => {
+        playRound('rock');
+        if (rounds == 4) {
+        playGame()
+        }
+        else {rounds++};
+    });
+    paperb.addEventListener("click", () => {
+        playRound('paper');
+        if (rounds == 4) {
+        playGame()
+        }
+        else {rounds++};
+    });
+    scissorsb.addEventListener("click", () => {
+        playRound('scissors');
+        if (rounds == 4) {
+        playGame()
+        }
+        else {rounds++};
+    });
+
+function playRound(humanSelection) {
+    const computerSelection = getComputerChoice()
+    if (humanSelection == "rock" && computerSelection == "scissors" ||
+        humanSelection == 'paper' && computerSelection == 'rock' ||
+        humanSelection == 'scissors' && computerSelection == 'paper') {
+            console.log(wonRound);
+            humanScore++
+        }
+        else if (humanSelection == "rock" && computerSelection == "rock" ||
+            humanSelection == "paper" && computerSelection == "paper" ||
+            humanSelection == "scissors" && computerSelection == "scissors") {
+                console.log(tieRound);
+            }
+            else {
+                console.log(loseRound);
+                ComputerScore++
+            }
+            console.log("Your score" + ": " + humanScore);
+            console.log("Computer score" + ": " + ComputerScore);
+            console.log("");
 }
 
-let wonRound = "You victory!";
-let loseRound = "Computer victory!";
-let tieRound = 'Tie, try again';
-
 function playGame() {
-    let humanScore = 0;
-    let ComputerScore = 0;
-    function playRound() {
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        if (humanSelection == "rock" && computerSelection == "scissors" ||
-            humanSelection == 'paper' && computerSelection == 'rock' ||
-            humanSelection == 'scissors' && computerSelection == 'paper') {
-                console.log(wonRound);
-                humanScore++
-            }
-            else if (humanSelection == "rock" && computerSelection == "rock" ||
-                humanSelection == "paper" && computerSelection == "paper" ||
-                humanSelection == "scissors" && computerSelection == "scissors") {
-                    console.log(tieRound);
-                }
-                else {
-                    console.log(loseRound);
-                    ComputerScore++
-                }
-                console.log("Your score" + ": " + humanScore);
-                console.log("Computer score" + ": " + ComputerScore);
-                console.log("");
-    }
-    playRound();
-    playRound();
-    playRound();
-    playRound();
-    playRound();
     if (humanScore > ComputerScore) {
         console.log("You win with: " + humanScore + " Score");
         console.log("Computer score: " + ComputerScore);
